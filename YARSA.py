855d6e891ca94e87305c65bab0653d6bffc5241e
Jatin Luthra <jatinluthra14@gmail.com>
2019-07-01 16:52:57 +0530

    Add flag-format option and pep8

--- YARSA.py
+++ YARSA.py
@@ -9,20 +9,22 @@ import primefac
 from contextlib import contextmanager
 import pyperclip
 
+
 @contextmanager
 def suppress_stdout():
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
-        try:  
+        try:
             yield
         finally:
             sys.stdout = old_stdout
 
+
 def extract_params(**kwargs):
     params = dict()
     if 'params_file' in kwargs:
-        with open(kwargs['params_file'],"r") as fp:
+        with open(kwargs['params_file'], "r") as fp:
             file_contents = fp.read()
     elif 'clipboard' in kwargs:
         file_contents = pyperclip.paste()
@@ -38,35 +40,35 @@ def extract_params(**kwargs):
     q = re.search('[q|Q]\s*[:=()]+\s*(.*)', file_contents)
     m = re.search('[m|M]\s*[:=()]+\s*(.*)', file_contents)
     e = re.search('[e|E]\s*[:=()]+\s*(.*)', file_contents)
-    c = re.search('[c|C]\s*[:=()]+\s*(.*)', file_contents)    
+    c = re.search('[c|C]\s*[:=()]+\s*(.*)', file_contents)
     if n:
         if n.group(1).startswith('0x'):
-            params['n'] = int(n.group(1),16)
+            params['n'] = int(n.group(1), 16)
         else:
             params['n'] = int(n.group(1))
     if p:
         if p.group(1).startswith('0x'):
-            params['p'] = int(p.group(1),16)
+            params['p'] = int(p.group(1), 16)
         else:
             params['p'] = int(p.group(1))
     if q:
         if q.group(1).startswith('0x'):
-            params['q'] = int(q.group(1),16)
+            params['q'] = int(q.group(1), 16)
         else:
             params['q'] = int(q.group(1))
     if m:
         if m.group(1).startswith('0x'):
-            params['m'] = int(m.group(1),16)
+            params['m'] = int(m.group(1), 16)
         else:
             params['m'] = int(m.group(1))
     if e:
         if e.group(1).startswith('0x'):
-            params['e'] = int(e.group(1),16)
+            params['e'] = int(e.group(1), 16)
         else:
             params['e'] = int(e.group(1))
     if c:
         if c.group(1).startswith('0x'):
-            params['c'] = int(c.group(1),16)
+            params['c'] = int(c.group(1), 16)
         else:
             params['c'] = int(c.group(1))
     return params
@@ -79,11 +81,11 @@ class YARSA:
         self.c = None
         self.d = None
         self.phi = None
-        self.m =None
+        self.m = None
         self.args = args
         self.__dict__.update(kwargs)
         self.session = requests.session()
-        gmpy2.get_context().precision=50000
+        gmpy2.get_context().precision = 50000
 
     def factorize(self):
         self.print_not_silent("Finding Primes on factordb")
@@ -95,7 +97,7 @@ class YARSA:
                 return True
             else:
                 return False
-    
+
     def find_primes(self):
         try:
             list_primes = list(primefac.primefac(self.n))
@@ -109,10 +111,10 @@ class YARSA:
         except:
             return False
 
-            
     def factordb(self):
         base_url = "http://factordb.com/api"
-        results = self.session.get(base_url, params={"query": str(self.n)}).json()
+        results = self.session.get(
+            base_url, params={"query": str(self.n)}).json()
         if results['status'] != 'CF' and results['status'] != 'FF':
             return False
         else:
@@ -133,7 +135,7 @@ class YARSA:
         else:
             self.print_not_silent("Couldn't Decrypt")
             return False
-    
+
     def formatted(self, s):
         return f"-----------------------------------------------------------\nDEC: {self.m}\nHEX: {hex(self.m)}\nASCII: {long_to_bytes(self.m).decode('utf-8', errors='ignore')}\n-----------------------------------------------------------"
 
@@ -147,9 +149,9 @@ class YARSA:
             self.print_not_silent('Hit Wiener Attack!')
             self.print_dec()
             exit(0)
-    
+
     def small_e(self):
-        if self.e in range(1,18):
+        if self.e in range(1, 18):
             root = gmpy2.root(self.c, self.e)
             if root > gmpy2.root(self.n, self.e):
                 return False
@@ -176,23 +178,55 @@ class YARSA:
             print(s)
 
     def print_dec(self):
-        result = self.formatted(self.m)
-        print(result)
+        if self.args.flag_format:
+            try:
+                self.flag = re.search(
+                    f"({self.args.flag_format})", self.formatted(self.m)).group(1)
+                self.print_not_silent(
+                    f"Found a flag in plaintext:\n------------------")
+                print(self.flag)
+                self.print_not_silent("------------------")
+                if not self.args.silent:
+                    opt = input(
+                        'Do you still want to see plaintext(s)? (y/N): ')
+                    if opt:
+                        if opt.lower()[0] == 'y':
+                            self.print_not_silent(
+                                "Okay, Printing Plaintext(s)")
+                            print(self.formatted(self.m))
+                            exit(0)
+                    self.print_not_silent(
+                        "Copying the flag to clipboard!")
+                pyperclip.copy(self.flag)
+                exit(0)
+            except Exception as e:
+                self.print_not_silent(e)
+        self.print_not_silent("Found Plaintext(s): ")
+        print(self.formatted(self.m))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Yet Another RSA Toolkit")
-    parser.add_argument('-pf','--params-file', help='file which store params like n, e, c and/or others')
-    parser.add_argument('-cp','--clipboard', help='copy params from clipboard automatically', action='store_true')
-    parser.add_argument('-na','--no-attacks', help='skip attacks and try old school RSA decryption', action='store_true')
-    parser.add_argument('-lp','--list-primes', help='list all the prime factors if found', action='store_true')
-    parser.add_argument('-s','--silent', help='just print the plaintext', action='store_true')
+    parser.add_argument('-pf', '--params-file',
+                        help='file which store params like n, e, c and/or others')
+    parser.add_argument(
+        '-cp', '--clipboard', help='copy params from clipboard automatically', action='store_true')
+    parser.add_argument('-na', '--no-attacks',
+                        help='skip attacks and try old school RSA decryption', action='store_true')
+    parser.add_argument('-lp', '--list-primes',
+                        help='list all the prime factors if found', action='store_true')
+    parser.add_argument(
+        '-s', '--silent', help='just print the plaintext', action='store_true')
+    parser.add_argument('-ff', '--flag-format',
+                        help='The known flag format(regex) to search for, otherwise displays everything')
     args = parser.parse_args()
-    
+
     if not args.params_file and not args.clipboard:
         print("Please specify the params!")
         exit(0)
     elif args.params_file:
-        params = extract_params(params_file=args.params_file, silent=args.silent)
+        params = extract_params(
+            params_file=args.params_file, silent=args.silent)
     else:
         params = extract_params(clipboard=True, silent=args.silent)
     yarsa = YARSA(args, **params)
